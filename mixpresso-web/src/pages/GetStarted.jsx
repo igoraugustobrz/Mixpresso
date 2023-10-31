dc7dc9db68e40014f5639eae4e78425cf7489e6e
Igor Augusto <igoraugustobz@gmail.com>
2023-10-30 22:56:08 -0300

    feat: adicionada a versão desktop da página GetStarted

--- mixpresso-web/src/pages/GetStarted.jsx
+++ mixpresso-web/src/pages/GetStarted.jsx
@@ -1,38 +1,53 @@
 'use client';
 import { Button } from 'flowbite-react';
-import homeMobile from '../assets/mobile.png';
+import homeMobile from '../assets/global/mobile.png';
+import homeDesktop from '../assets/global/desktop.jpg';
+import { Link } from 'react-router-dom';
 
 const GetStarted = () => {
     return (
         <>
-            <div className='relative sm:hidden w-screen h-screen'>
-                <div>
+            <div className='relative w-full h-screen sm:h-full'>
+                <div className='block sm:hidden'>
                     <img
-                        className='w-full h-[90vh]'
+                        className='w-full h-90vh'
                         src={homeMobile}
                         alt="Home"
                     />
                 </div>
 
-                <div className='content'>
-                    <div className='h-[40vh] bg-black-white'>
-                        <div className='content-inner'>
-                            <p className='main-text'>Café tão bom, que seu paladar vai amar.</p>
+                <div className='hidden sm:block'>
+                    <img
+                        className='w-full h-[90vh]'
+                        src={homeDesktop}
+                        alt='Home'
+                    />
+                </div>
 
-                            <p className='sub-text'>O melhor grão, a torra mais fina, o sabor poderoso</p>
+            </div>
 
+            <div className='content'>
+                <div className='h-[40vh] bg-black-white'>
+                    <div className='content-inner'>
+                        <p className='main-text'>Café tão bom, que seu paladar vai amar.</p>
+
+                        <p className='sub-text'>O melhor grão, a torra mais fina, o sabor poderoso</p>
+
+                        <Link
+                            to='/home'
+                        >
                             <Button
-                            size="sm"
-                                className='mx-auto px-10'
+                                size="sm"
+                                className='mx-auto px-10 sm:px-20 sm:text-9xl sm:py-2'
                                 color="warning"
                             >
                                 Vamos lá!
                             </Button>
-                        </div>
+                        </Link>
                     </div>
-
                 </div>
-            </div >
+
+            </div>
         </>
     );
 };
