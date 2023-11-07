12352562e5347f19e80c2d7d059ce898fc05683e
Igor Augusto <igoraugustobz@gmail.com>
2023-11-07 20:17:35 -0300

    feat: adição do Spring Boot ao projeto

---
+++ mixpresso-api/src/main/java/br/com/mixpresso/mixpressoapi/MixpressoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mixpresso.mixpressoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixpressoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixpressoApiApplication.class, args);
+	}
+
+}
---
+++ mixpresso-api/src/test/java/br/com/mixpresso/mixpressoapi/MixpressoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mixpresso.mixpressoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixpressoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
